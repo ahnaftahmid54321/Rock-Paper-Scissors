be037b343e9b59c7c557aa3b59150eb7d4da7d0d
Ahnaf Tahmid <ahnaft.official@gmail.com>
2025-11-14 20:55:48 -0800

    Added options for the variable plays

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
 function getComputerChoice() {
     const choices = ["Rock", "Paper", "Scissors"]
-    console.log(choices);
-    return Math.floor(Math.random()* 3);
-    
+    const index = Math.floor(Math.random() * choices.length)
+    console.log(choices[index]);
+    return choices[index];
 }
-    
+const playerMe = "Rock";
+const playerThem = getComputerChoice();
+
+if (playerMe === playerThem) {
+    console.log("It's a Tie");
+}
+else if (/*PlayerMe is "Rock" and PlayerThem is "Scissors"*/
+    playerMe = "Rock" && playerThem = "Scissors"
+    console.log("You Win!!"));
+
+else if (/*PlayerMe is "Scissors" and PlayerThem is "Rock")*/
+    playerMe = "Scissors" && playerThem = "Rock"
+    console.log("You Lose"));
+
+else if (/*PlayerMe is "Paper" and PlayerThem is "Scissors")*/
+    playerMe = "Paper" && playerThem = "Scissors"
+    console.log("You Lose"));
+
+else if (/*PlayerMe is "Rock" and PlayerThem is "Scissors")*/
+    playerMe = "Scissors" && playerThem = "Scissors"
+    console.log("You Lose"));
+
