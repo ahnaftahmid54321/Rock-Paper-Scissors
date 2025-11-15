c1bb9d555ad19609766632bf61af067230d03b91
Ahnaf Tahmid <ahnaft.official@gmail.com>
2025-11-14 22:28:13 -0800

    Logic is well. Need well placement of code for loopback

--- script.js
+++ script.js
@@ -3,10 +3,16 @@ function getComputerChoice() {
     const index = Math.floor(Math.random() * choices.length)
     console.log(choices[index]);
     return choices[index];
-}
-const playerMe = "Rock";
+} 
+const playerMe = prompt("Type Rock, Paper or Scissors");
 const playerThem = getComputerChoice();
+console.log("Me:", playerMe, "Them:", playerThem);
 
+function playRound() {
+    const playerMe = prompt("Type Rock, Paper or Scissors");
+    const playerThem = getComputerChoice();
+    console.log("Me:", playerMe, "Them:", playerThem);
+}
 if (playerMe === playerThem) {
     console.log("It's a Tie");
 }
@@ -23,10 +29,18 @@ else if (playerMe === "Paper" && playerThem === "Scissors") {
     console.log("You Lose");
 }
 /*PlayerMe is "Rock" and PlayerThem is "Scissors")*/
-else if (playerMe === "Scissors" && playerThem === "Scissors") {
-    console.log("You Lose");
+else if (playerMe === "Paper" && playerThem === "Rock") {
+    console.log("You Win!!");
 }
 /*PlayerMe is "Scissors" and PlayerThem is "Rock")*/
 else if (playerMe === "Scissors" && playerThem === "Rock") {
     console.log("You Lose");
+}
+/*PlayerMe is "Scissors" and PlayerThem is "Paper")*/
+else if (playerMe === "Scissors" && playerThem === "Paper") {
+    console.log("You Win!!");
+}
+const again = prompt("Play again? (yes/no)");
+if (again && again.toLowerCase() === "yes") {
+playRound();
 }
